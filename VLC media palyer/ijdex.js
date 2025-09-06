1341432a4f1f46d9d5cf3d9d2c8dcacde7d71d91
Aditya singh <adityasingh14.2019@gmail.com>
2025-09-06 12:20:45 +0530

    done

--- VLC media palyer/ijdex.js
+++ VLC media palyer/ijdex.js
@@ -2,14 +2,26 @@ alert("js added");
 // window.onload = ()=>{
 //     alert("page loaded");
 // }
-const SpeedUp = document.getElementById("SpeedUp");
-const SpeedDown = document.getElementById("SpeedDown");
-const Volumeup = document.getElementById("Volumeup");
-const volumedown = document.getElementById("volumedown");
+const SpeedUp = document.querySelector("#SpeedUp");
+const SpeedDown = document.querySelector("#SpeedDown");
+const Volumeup = document.querySelector("#VolumeUp");
+const volumedown = document.querySelector("#VolumeDown");
+const video_btn = document.querySelector("#open-btn");
+const video_input = document.querySelector("#video_input");
 const speedhandler = function () {
   alert("Speed Up");
 };
 SpeedUp.addEventListener("click", speedhandler);
 SpeedDown.addEventListener("click", () => {
   alert("Speed Down");
+});
+
+Volumeup.addEventListener("click", () => {
+  alert("Volume Up");
+});
+volumedown.addEventListener("click", () => {
+  alert("Volume Down");
+});
+video_btn.addEventListener("click", () => {
+  video_input.click();
 });
