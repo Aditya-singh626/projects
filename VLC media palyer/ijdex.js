5ac92dc1536646aeb536f142b48ab7a4ea271674
Aditya singh <adityasingh14.2019@gmail.com>
2025-09-07 22:49:30 +0530

    just comment them to know the problem

--- VLC media palyer/ijdex.js
+++ VLC media palyer/ijdex.js
@@ -8,22 +8,22 @@ const Volumeup = document.querySelector("#VolumeUp");
 const volumedown = document.querySelector("#VolumeDown");
 const video_btn = document.querySelector("#open-btn");
 const video_input = document.querySelector("#video_input");
-console(SpeedUp);
-video_btn.addEventListener("click", () => {
-  video_input.click();
-});
-SpeedUp.addEventListener("click", () => {
-  alert("Speed Up");
-});
-SpeedDown.addEventListener("click", () => {
-  alert("Speed Down");
-});
+console.log(SpeedUp);
+// video_btn.addEventListener("click", () => {
+//   video_input.click();
+// });
+// SpeedUp.addEventListener("click", () => {
+//   alert("Speed Up");
+// });
+// SpeedDown.addEventListener("click", () => {
+//   alert("Speed Down");
+// });
 
-Volumeup.addEventListener("click", () => {
-  alert("Volume Up");
-});
-volumedown.addEventListener("click", () => {
-  alert("Volume Down");
-});
+// Volumeup.addEventListener("click", () => {
+//   alert("Volume Up");
+// });
+// volumedown.addEventListener("click", () => {
+//   alert("Volume Down");
+// });
 
 
