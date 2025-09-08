936525fab002a5442350b1087b9efcf5ee663601
Aditya singh <adityasingh14.2019@gmail.com>
2025-09-08 12:34:14 +0530

    just learned some thing nem about js .

--- VLC media palyer/ijdex.js
+++ VLC media palyer/ijdex.js
@@ -9,21 +9,21 @@ const volumedown = document.querySelector("#VolumeDown");
 const video_btn = document.querySelector("#open-btn");
 const video_input = document.querySelector("#video_input");
 console.log(SpeedUp);
-// video_btn.addEventListener("click", () => {
-//   video_input.click();
-// });
-// SpeedUp.addEventListener("click", () => {
-//   alert("Speed Up");
-// });
-// SpeedDown.addEventListener("click", () => {
-//   alert("Speed Down");
-// });
+video_btn.addEventListener("click", () => {
+  video_input.click();
+});
+SpeedUp.addEventListener("click", () => {
+  alert("Speed Up");
+});
+SpeedDown.addEventListener("click", () => {
+  alert("Speed Down");
+});
 
-// Volumeup.addEventListener("click", () => {
-//   alert("Volume Up");
-// });
-// volumedown.addEventListener("click", () => {
-//   alert("Volume Down");
-// });
+Volumeup.addEventListener("click", () => {
+  alert("Volume Up");
+});
+volumedown.addEventListener("click", () => {
+  alert("Volume Down");
+});
 
 
