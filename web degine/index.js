a609e852e217df5d338f6ee03c9468e507bec2dc
Aditya singh <adityasingh14.2019@gmail.com>
2025-09-14 23:06:23 +0530

    13 step 2025

--- web degine/index.js
+++ web degine/index.js
@@ -6,6 +6,6 @@ file.addEventListener("click", () => {
 resume.addEventListener("change", (obj) => {
   console.log("obj", obj);
   const doc = obj.target.files[0];
-  //   const doc2 = document.createElement("file");
-  //   file.src = URL.createObjectURL(doc);
+    const doc2 = document.createElement("file");
+    file.src = URL.createObjectURL(doc);
 });
