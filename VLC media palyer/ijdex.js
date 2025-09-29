ffca03a12fdd40ef84cb78ece84bf7b57bae1e4e
Aditya singh <adityasingh14.2019@gmail.com>
2025-09-29 23:03:16 +0530

    done

--- VLC media palyer/ijdex.js
+++ VLC media palyer/ijdex.js
@@ -8,14 +8,18 @@ const Volumeup = document.querySelector("#VolumeUp");
 const volumedown = document.querySelector("#VolumeDown");
 const video_btn = document.querySelector("#open-btn");
 const video_input = document.querySelector("#video_input");
+const video_player = document.querySelector("#main");
 video_btn.addEventListener("click", () => {
   video_input.click();
 });
 video_input.addEventListener("change", (obj) => {
   console.log("obj", obj);
   const file = obj.target.files[0];
+  //scr->base64
+  const link = URL.createObjectURL(file);
   const video = document.createElement("video");
-  video.src = URL.createObjectURL(file);
+  video.src = link;
+  video_player.appendChild (video);
 });
 SpeedUp.addEventListener("click", () => {
   alert("Speed Up");
