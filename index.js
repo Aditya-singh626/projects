38838226380d6668d5eb7fa9d2a8a79e95c0b9fb
Aditya singh <adityasingh14.2019@gmail.com>
2025-09-30 22:57:19 +0530

    adding todo list

---
+++ index.js
@@ -0,0 +1,5 @@
+const buttton = document.querySelector('button');
+const box = document.querySelector('.box');
+buttton.addEventListener('click', () => {
+    box.style.backgroundColor = 'red';
+});
